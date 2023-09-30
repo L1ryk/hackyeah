53d756f6cb237034dbb3d4eb9313441403e10e3b
daruyami <shadedgreen65@gmail.com>
2023-09-30 21:09:29 +0200

    courses

--- WebAPI/DataSource/Entities/Univerisites/Course.cs
+++ WebAPI/DataSource/Entities/Univerisites/Course.cs
@@ -5,4 +5,6 @@ public class Course
     public Guid Id { get; set; }
 
     public string Name { get; set; }
+    
+    public List<CourseTag> Tags { get; set; }
 }
---
+++ WebAPI/Models/Responses/UniversityCourses/GetUniversityCourses.cs
@@ -0,0 +1,17 @@
+﻿using WebAPI.DataSource.Entities.Univerisites;
+using WebAPI.Models.Paginations;
+
+namespace WebAPI.Models.Responses.Courses;
+
+public class GetUniversityCourses : Pagination
+{
+    public Guid? Id { get; set; }
+
+    public University? University { get; set; }
+
+    public Course? Course { get; set; }
+
+    public string? Language { get; set; }
+
+    public string? Profile { get; set; }
+}
