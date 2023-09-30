d55ce231a8253cfbf34b9b48abfdc67f2ede6bac
Łukasz Boruń <l.borun@xopero.com>
2023-09-30 13:58:42 +0200

    added error handler, global response dto

---
+++ WebAPI/Helpers/ErrorHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLog;
+using WebAPI.Models.Responses;
+
+namespace WebAPI.Helpers;
+
+public class ErrorHandler
+{
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    public static async Task< ActionResult > HandlerAsync( Func< Task< ActionResult > > caller )
+    {
+        try
+        {
+            return await caller();
+        }
+        catch( Exception ex )
+        {
+            _logger.Error( ex );
+
+            return new JsonResult( new Response
+            {
+                IsSuccess = false,
+                Error = ex.Message
+            } );
+        }
+    }
+}
---
+++ WebAPI/Models/Responses/Response.cs
@@ -0,0 +1,30 @@
+﻿namespace WebAPI.Models.Responses
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Error { get; set; }
+    }
+
+    public class Response< T > : Response
+    {
+        /// <summary> Empty constructor </summary>
+        public Response()
+        {
+        }
+
+        /// <summary> Create new success response </summary>
+        /// <param name="result"></param>
+        public Response( T result )
+        {
+            Result = result;
+            IsSuccess = true;
+        }
+
+        /// <summary>
+        /// Returned data
+        /// </summary>
+        public T Result { get; set; }
+    }
+}
