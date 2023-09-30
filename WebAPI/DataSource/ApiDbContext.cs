47f0c3846df691406f4e7a769e4990ba3917079a
Dawid Lewandowski <d.lewandowski@xopero.com>
2023-09-30 12:31:45 +0200

    ef

---
+++ WebAPI/DataSource/ApiDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.DataSource.Entities.System;
+
+namespace WebAPI.DataSource;
+
+public class ApiDbContext : DbContext
+{
+    #region System
+
+    public DbSet< User > Users { get; set; }
+
+    #endregion
+
+    #region constructors and boilerplate
+
+    public ApiDbContext( DbContextOptions< ApiDbContext > options )
+        : base( options )
+    {
+    }
+
+    protected override void OnModelCreating( ModelBuilder modelBuilder )
+    {
+        base.OnModelCreating( modelBuilder );
+    }
+
+    protected override void OnConfiguring( DbContextOptionsBuilder options )
+    {
+    }
+
+    #endregion
+}
---
+++ WebAPI/DataSource/Entities/System/User.cs
@@ -0,0 +1,6 @@
+﻿namespace WebAPI.DataSource.Entities.System;
+
+public class User
+{
+    public Guid Id { get; set; }
+}
