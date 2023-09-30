cfcb33f232b88e49f3b2059f2e932810d28804c3
Dawid Lewandowski <d.lewandowski@xopero.com>
2023-09-30 21:28:13 +0200

    tagimporter....

---
+++ Scrap/Scrappers/TagImporter.cs
@@ -0,0 +1,15 @@
+﻿namespace Scrap.Scrappers;
+
+public class TagImporter
+{
+    private readonly string path;
+
+    public TagImporter( string path )
+    {
+        this.path = path;
+    }
+
+    public async Task Import()
+    {
+    }
+}
