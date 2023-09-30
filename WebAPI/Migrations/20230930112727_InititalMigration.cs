bad2df786b46670f6be56de4cd3f1c59d99bb984
Łukasz Boruń <l.borun@xopero.com>
2023-09-30 13:27:38 +0200

    added migrations

---
+++ WebAPI/Migrations/20230930112727_InititalMigration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InititalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Universities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Universities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voivodeships",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voivodeships", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Universities");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Voivodeships");
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,9 @@
-var builder = WebApplication.CreateBuilder( args );
+using Microsoft.EntityFrameworkCore;
+using NLog;
+using WebAPI.DataSource;
+
+var logger = LogManager.GetCurrentClassLogger();
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -7,10 +12,29 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var dbConnection = Environment.GetEnvironmentVariable("DbConnection");
+
+builder.Services.AddDbContext<ApiDbContext>(options =>
+{
+    options.UseNpgsql(dbConnection,
+        optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(ApiDbContext).Assembly.FullName));
+});
+
 var app = builder.Build();
 
+try
+{
+    using var services = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+    var dbc = services.ServiceProvider.GetService<ApiDbContext>();
+    dbc?.Database.Migrate();
+}
+catch (Exception ex)
+{
+    logger.Error(ex, "Problem with DB initialization");
+}
+
 // Configure the HTTP request pipeline.
-if ( app.Environment.IsDevelopment() )
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
